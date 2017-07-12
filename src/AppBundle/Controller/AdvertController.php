7e511fdeaaeced91b346de427ef8d341c0cf8b36
Samuel Besnier <sbesnier1901@gmail.com>
2017-07-12 13:37:02 +0200

    add search filter

--- src/AppBundle/Controller/AdvertController.php
+++ src/AppBundle/Controller/AdvertController.php
@@ -3,8 +3,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route(
@@ -62,6 +66,95 @@ class AdvertController extends Controller
 
         return $this->render('AppBundle:Advert:index.html.twig', [
             "adverts" => $adverts
+        ]);
+    }
+
+    /**
+     * @Route(
+     *     "/search",
+     *     name="search"
+     * )
+     */
+    public function searchAction(Request $request)
+    {
+        $defaultData = array('message' => 'Formulaire de recherche');
+
+        $form = $this->createFormBuilder($defaultData)
+            ->add(
+                'project',
+                EntityType::class,
+                [
+                    "class" => "AppBundle\Entity\Project",
+                    "choice_label" => "project",
+                    "placeholder" => "Choisissez un projet",
+                    "label" => "Projet",
+                    "required" => false
+                ])
+            ->add(
+                'category',
+                EntityType::class,
+                [
+                    "class" => "AppBundle\Entity\Category",
+                    "choice_label" => "category",
+                    "placeholder" => "Choisissez une catégorie",
+                    "label" => "Catégorie",
+                    "required" => false
+                ]
+            )
+            ->add(
+                'minPrice',
+                IntegerType::class,
+                [
+                    "label" => "Prix minimum",
+                    "required" => false
+                ])
+            ->add(
+                'maxPrice',
+                IntegerType::class,
+                [
+                    "label" => "Prix maximum",
+                    "required" => false
+                ]
+            )
+            ->add(
+                'postcode',
+                IntegerType::class,
+                [
+                    "label" => "Code postal",
+                    "required" => false
+                ]
+            )
+            ->add(
+                'submit',
+                SubmitType::class,
+                [
+                    "label" => "Rechercher"
+                ]
+            )
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $project = $data['project'];
+            $category = $data['category'];
+            $minPrice = $data['minPrice'];
+            $maxPrice = $data['maxPrice'];
+            $postCode = $data['postcode'];
+
+            $repo = $this->getDoctrine()->getRepository('AppBundle:Advert');
+
+            $adverts = $repo->searchAdverts($project, $category, $minPrice, $maxPrice, $postCode);
+
+            return $this->render('AppBundle:Advert:index.html.twig', [
+                "adverts" => $adverts
+            ]);
+        }
+
+        return $this->render('AppBundle:Advert:search.html.twig', [
+            "formSearch" => $form->createView()
         ]);
     }
 
--- src/AppBundle/Repository/AdvertRepository.php
+++ src/AppBundle/Repository/AdvertRepository.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Project;
 use AppBundle\Entity\User;
 
 
@@ -44,5 +46,86 @@ class AdvertRepository extends \Doctrine\ORM\EntityRepository
             ->setParameter('user', $user->getId());
 
         return $query->getResult();
+    }
+
+    public function searchAdverts($project, $category, $minPrice, $maxPrice, $postCode)
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->select('a')
+            ->innerJoin('a.images', 'images')
+            ->innerJoin('a.address', 'address');
+
+        $hasParameters = false;
+
+        if (!empty($project)) {
+            $qb = $this->setQueryParameters(
+                $project,
+                [
+                    "condition" => "a.project = :project",
+                    "objName" => "project"
+                ],
+                $qb,
+                $hasParameters);
+            $hasParameters = true;
+        }
+        if (!empty($category)) {
+            $qb = $this->setQueryParameters(
+                $category,
+                [
+                    "condition" => "a.category = :category",
+                    "objName" => "category"
+                ],
+                $qb,
+                $hasParameters);
+            $hasParameters = true;
+        }
+        if (!empty($minPrice)) {
+            $qb = $this->setQueryParameters(
+                $minPrice,
+                [
+                    "condition" => "a.price > :minPrice",
+                    "objName" => "minPrice"
+                ],
+                $qb,
+                $hasParameters);
+            $hasParameters = true;
+        }
+        if (!empty($maxPrice)) {
+            $qb = $this->setQueryParameters(
+                $maxPrice,
+                [
+                    "condition" => "a.price < :maxPrice",
+                    "objName" => "maxPrice"
+                ],
+                $qb,
+                $hasParameters);
+            $hasParameters = true;
+        }
+        if (!empty($postCode)) {
+            $qb = $this->setQueryParameters(
+                $postCode,
+                [
+                    "condition" => "a.postcode = :postcode",
+                    "objName" => "postcode"
+                ],
+                $qb,
+                $hasParameters);
+            $hasParameters = true;
+        }
+        $query = $qb->getQuery();
+
+        return $query->getResult();
+    }
+
+    private function setQueryParameters($obj, $options, $qb, $hasParams)
+    {
+        if ($hasParams) {
+            $qb->andWhere($options['condition']);
+        } else {
+            $qb->where($options['condition']);
+        }
+        $qb->setParameter($options['objName'], $obj);
+
+        return $qb;
     }
 }
